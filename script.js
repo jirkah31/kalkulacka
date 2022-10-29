05f3ff6cd1c57acf52de66acdd87c8fc2fce67ce
Jiří Hruška <jirkah.31@seznam.cz>
2022-10-29 14:01:36 +0200

    funkční kalkulačka, je nutné odladit vyjímky a roztahování displaye

--- script.js
+++ script.js
@@ -2,30 +2,64 @@ const button = document.querySelector(".buttons");
 
 const display = document.querySelector("#screen");
 
+const h2 = document.querySelector('h2');
+
+const example = [];
+
 button.addEventListener("click", (event) => {
 	const keyValue = event.target.value;
-	console.log(keyValue);
 
-	const displayValue = display.textContent;
-	console.log(displayValue);
+	const displayValue = h2.innerText;
+	//když mačkám čísla
+	if (keyValue != undefined &&
+		keyValue != "+" &&
+		keyValue != "-" &&
+		keyValue != "/" &&
+		keyValue != "*" &&
+		keyValue != "+/-" &&
+		keyValue != "=" &&
+		keyValue != "CE") 
+		{
+		if (displayValue == 0) { 			//když je hodnota 0, tak aby to přepsalo display
+			h2.innerText = keyValue;
+			cislo = h2.innerText;
+		} else {							//pokračování zapisování do displaye
+			h2.innerText = displayValue + keyValue;
+			cislo = h2.innerText;
+		}
+	}
 
-	if (displayValue == 0) {
-		display.textContent = keyValue;
-	} else {
-		display.textContent = displayValue + keyValue;
+	if (keyValue == "+" ||
+		keyValue == "-" ||
+		keyValue == "/" ||
+		keyValue == "*") {
+		cislo = h2.innerText;
+		example.push(cislo);
+		mark = event.target.value;
+		example.push(mark);
+		h2.innerText = 0;
+	}
+
+	if (keyValue == "=") {
+		cislo = h2.innerText;
+		example.push(cislo);
+		var result = eval(example.join(''));
+		h2.innerText = result;
+
+		for (var i = example.length; 0 < i; i--) {
+			example.pop();
+		}
+	}
+
+	if (keyValue == "CE") {
+		for (var i = example.length; 0 < i; i--) {
+			example.pop();
+		}
+		h2.innerText = 0;
+	}
+	
+	if (keyValue == "+/-") {
+		cislo = h2.innerText * (-1);
+		h2.innerText = cislo;
 	}
 });
-
-
-	// document.addEventListener("click", (udalost) => {
-	// 	const one1 = one.value; //vrací mi číslo, které jsem zmáčknul
-	// 	console.log(one1);
-	// });
-
-	// two.addEventListener("click", (udalost) => {
-	// 	const two2 = two.value;
-	// 	console.log(two2);
-	// });
-
-	// const screen = document.getElementById("screen");
-	// screen.innerText = "cislo";
