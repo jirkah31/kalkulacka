354986991122c31020f8f4827b9516632bc8dabb
Jiří Hruška <jirkah.31@seznam.cz>
2022-10-29 15:54:45 +0200

    nastavení tlačítka del

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const button = document.querySelector(".buttons");
 
-const display = document.querySelector("#screen");
-
 const h2 = document.querySelector('h2');
 
 const example = [];
@@ -10,7 +8,7 @@ button.addEventListener("click", (event) => {
 	const keyValue = event.target.value;
 
 	const displayValue = h2.innerText;
-	//když mačkám čísla
+
 	if (keyValue != undefined &&
 		keyValue != "+" &&
 		keyValue != "-" &&
@@ -18,14 +16,15 @@ button.addEventListener("click", (event) => {
 		keyValue != "*" &&
 		keyValue != "+/-" &&
 		keyValue != "=" &&
-		keyValue != "CE") 
-		{
-		if (displayValue == 0) { 			//když je hodnota 0, tak aby to přepsalo display
-			h2.innerText = keyValue;
-			cislo = h2.innerText;
-		} else {							//pokračování zapisování do displaye
-			h2.innerText = displayValue + keyValue;
-			cislo = h2.innerText;
+		keyValue != "CE") {
+		if (h2.innerText.length < 10) {
+			if (displayValue == 0) {
+				h2.innerText = keyValue;
+				cislo = h2.innerText;
+			} else {
+				h2.innerText = displayValue + keyValue;
+				cislo = h2.innerText;
+			}
 		}
 	}
 
@@ -57,9 +56,13 @@ button.addEventListener("click", (event) => {
 		}
 		h2.innerText = 0;
 	}
-	
+
 	if (keyValue == "+/-") {
 		cislo = h2.innerText * (-1);
 		h2.innerText = cislo;
+	}
+
+	if (keyValue == "del") {
+		h2.innerText = 0;
 	}
 });
